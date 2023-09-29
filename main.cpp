b6fc950b3d32a0603c1e2c28bca969b145bc4dac
igorpadua <igorpadua@tutanota.com>
2023-09-29 11:55:22 -0300

    refactor: cppcheck

--- main.cpp
+++ main.cpp
@@ -7,13 +7,14 @@
 auto main(int argc, char** argv) -> int
 {
     if (argc > 1) {
-        std::string line = { "" };
         std::ifstream file(argv[1]);
-        const char* delim = ",";
-        std::array<std::string, 4> fields { "NAME", "YEAR", "LANG", "MAIL" };
-        int i = {};
 
         if (file.is_open()) {
+            std::string line = { "" };
+            const char* delim = ",";
+            std::array<std::string, 4> fields { "NAME", "YEAR", "LANG", "MAIL" };
+            int i = {};
+
             while (std::getline(file, line)) {
                 char* buf = &line[0];
                 char* pline = strsep(&buf, delim);
